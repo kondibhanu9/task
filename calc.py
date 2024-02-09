630dbcdd72dbe676a8b9fe4fcc4a0938620884d7
kondibhanu9 <84490805+kondibhanu9@users.noreply.github.com>
2024-02-09 11:44:54 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,10 +4,13 @@ def subtraction(a,b):
     print(a-b)
 def multiplication(a,b):
     print(a*b)
+def division(a,b):
+    print(a//b)
 a=5
 b=6
 addition(a,b)
 subtraction(a,b)
 multiplication(a,b)
+division(a,b)
 print("Updated")
 print("Updated file")
